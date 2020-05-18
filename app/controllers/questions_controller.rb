8e74ced358c2d6cca25eaa9d759f49bab7cc745b
Julia Schwenner <julia.schwenner@gmail.com>
2020-05-18 15:07:31 +0200

    rails pages

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,2 +1,9 @@
 class QuestionsController < ApplicationController
+    def ask
+        @message = "Hello, please type your question here!"
+    end
+
+    def answer
+        @answer = "Here is your answer!"
+    end
 end
