393ec5e0bcfdfd2fb44cf4dd780ad1d4c887a272
Julia Schwenner <julia.schwenner@gmail.com>
2020-05-18 17:03:40 +0200

    rails pages

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,31 @@
 class QuestionsController < ApplicationController
     def ask
-        @message = "Hello, please type your question here!"
     end
 
     def answer
-        @answer = "Here is your answer!"
+
+        @question = params[:ask]
+    
+        if @question == "I am going to work"
+             @answer = "Great!"
+        elsif @question.ends_with?("?")
+            @answer = "Silly question, get dressed and go to work!"
+        else 
+            @answer = "I don't care, get dressed and go to work!"
+        end
     end
 end
+
+
+
+# app/controllers/pages_controller.rb
+
+#class PagesController < ApplicationController
+ #   def contact
+  #    @members = [ 'thanh', 'dimitri', 'germain', 'damien', 'julien' ]
+  
+   #   if params[:member]
+    #    @members = @members.select { |member| member.start_with?(params[:member]) }
+     # end
+    #end
+  #end
